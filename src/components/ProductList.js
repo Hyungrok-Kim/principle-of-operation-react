cfb51b9af408fabfa1208d2495221ecb06aa3ebf
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-04-14 17:15:37 +0900

    commit

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,9 +1,39 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 export default function ProductList() {
+
+    const [posts, setPosts] = useState([]);
+
+    function buttonClicked() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+          .then(res => {
+              console.log(res.data);
+              setPosts(res.data);
+          });
+    }
+
+    useEffect(() => {
+        buttonClicked();
+    }, [])
+
     return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
+        <React.Fragment>
+            <p>
+                <button onClick={buttonClicked}>get data</button>
+            </p>
+            <ul>
+                {
+                    posts.map((prop, idx) =>  {
+                        return (
+                            <li key={idx}>
+                                <p>글 번호 : {prop.id}</p>
+                                <p>{prop.title}</p>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </React.Fragment>
     );
 }
